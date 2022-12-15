73f868d4d3d581a0a9d62f0fabcc5354ea4b08cf
Gwenaëlle Herissant <gh@pointligneplan.fr>
2022-12-15 17:26:20 +0100

    [react] created homepage with its components

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Banner from '../components/Banner';
+import CardsGenerator from '../components/CardGenerator';
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <Banner/>
+      <CardsGenerator/>
+    </div>
+  );
+}
