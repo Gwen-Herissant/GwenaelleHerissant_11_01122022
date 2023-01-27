b80997c45476d99343f7f1fc964a2323dbfd0431
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-01-27 11:24:17 +0100

    [styles] error page for data issues

--- src/pages/ErrorData.jsx
+++ src/pages/ErrorData.jsx
@@ -5,9 +5,9 @@ export default function ErrorData() {
 
   return (
     <div>
-      <div id="error-page">
-        <h1>404</h1>
-        <p>Oups! Les données n'ont pas été trouvées</p>
+      <div id="error-page" className="error-data">
+        <h1>Oups! Une erreur est survenue</h1>
+        <p>Les données n'ont pas été trouvées</p>
         <Link to={`home`}>Retourner sur la page d’accueil</Link>
       </div>
     </div>
